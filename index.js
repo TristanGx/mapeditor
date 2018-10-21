6430b54e02e0d73197bebfc91dbb22ba82648b05
Tristan <trra53@gmail.com>
2018-10-21 17:07:13 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express();
+import path from "path";
+
+app.set("PORT", process.env.PORT || 5000)
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "app/index.html"));
+});
+
+app.get("/js/main.js", (req, res) => {
+    res.sendFile(path.join(__dirname, "app/js/main.js"));
+})
+
+app.get("/css/main.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "app/css/main.css"));
+})
+
+app.listen(app.get("PORT"));
+console.log("Server is listening at port:" + app.get("PORT"));
