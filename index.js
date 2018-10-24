6ff47a81fa406490c4fab6bf158b0ccb2a250e1e
Tristan <trra53@gmail.com>
2018-10-24 10:42:41 +0800

    favicon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const favicon = require("server-favicon");
+const favicon = require("serve-favicon");
 
 app.set("PORT", process.env.PORT || 5000);
 app.use(favicon(__dirname + "/app/favicon.ico"));
